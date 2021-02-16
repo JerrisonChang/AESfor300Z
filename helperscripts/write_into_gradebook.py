6124fd4ed997991cd5bf7d65b98caaeac588c2d4
JerrisonChang <jerrisonChang@gmail.com>
2021-02-16 11:25:30 -0500

    split train test complete

--- helperscripts/write_into_gradebook.py
+++ helperscripts/write_into_gradebook.py
@@ -20,7 +20,7 @@ def single_out_category(base_gradebook: 'df', category: str) -> 'df':
     export_gb = base_gradebook.filter(items = ['Username', category, 'essay content'])
     return export_gb
 
-def convert_df_to_category(singled_cat: 'df', max_pts: str) -> 'df':
+def convert_pts_to_rank(singled_cat: 'df', max_pts: str) -> 'df':
     def determin_rank(scheme, point: float):
         for i, x in enumerate(scheme):
             min_, max_ = x
@@ -36,6 +36,25 @@ def convert_df_to_category(singled_cat: 'df', max_pts: str) -> 'df':
     singled_cat.iloc[:,1] = singled_cat.iloc[:,1].apply(lambda value: determin_rank(scheme, value))
     return singled_cat
 
+def split_train_test(single_cat: 'df', ratio= 0.8) -> tuple:
+    assert ratio < 1
+    masks = [single_cat.iloc[:,1] == i for i in [1,2,3,4]]
+ 
+    train = []
+    test = []
+    for mask in masks:
+        test_data_of_rank = single_cat[mask]
+        train_data = test_data_of_rank.sample(frac= ratio)
+
+        train.append(train_data)
+        test.append(test_data_of_rank)
+
+    concat_train = pd.concat(train).sort_index()
+    concat_test = pd.concat(test).sort_index()
+
+    return (concat_train, concat_test)
+    
+
 def add_content_into_exisiting_gradebook(exisiting_gradebook_path, new_content_directory: str):
     grade_book_df = pd.read_excel(exisiting_gradebook_path, engine='openpyxl')
     
@@ -50,19 +69,25 @@ def split_train_test_on_item(data_path:str , column_name: str):
     
     rank_column = df[column_name]
     distribution = collections.Counter(rank_column)
-    print(distribution)
     # split each rank in 
 
 if __name__ == "__main__":
-    ASSIGNMENT_DIR = 'ICSI-300Z_Assignment1_Normative_Ethics'
-    GRADE_BOOK_PATH = '2020_hw_DATA.xlsx'
-    EXISTING_GRADE_BOOK_PATH = 'hw1_entire_data_processed.xlsx'
+    # GRADE_BOOK_PATH = '2020_hw_DATA.xlsx'
+    # EXISTING_GRADE_BOOK_PATH = 'hw1_entire_data_processed.xlsx'
 
     base_gb_hw1 = pd.read_excel('ICSI-300Z-Fall2020.xlsx', sheet_name=1, engine='openpyxl')
     # print(base_gb_hw1.head)
     append_essay_content_to_gb(base_gb_hw1, 'essays/hw1_fa20')
     writing_gb = single_out_category(base_gb_hw1, 'quality of writing')
-    print(convert_df_to_category(writing_gb, '10pts'))
+    abc = convert_pts_to_rank(writing_gb, '10pts')
+    train_test_data = split_train_test(abc)
+    output_dir = './classifiers/quality_of_writing'
+    output_name = ['train', 'test']
+
+    for data, name in zip(train_test_data, output_name):
+        data.to_csv(f"{output_dir}/hw1_{name}.csv")
+    
+
     # abc = get_content_and_column(base_gb_hw1, 'quality of writing', 'essays/hw1_fa20')
     
     # print(abc)
