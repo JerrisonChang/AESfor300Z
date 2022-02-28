e87fb27c1d9a917f132a7cd28001bfc946b847c4
Jerrison Chang <jerrisonchang@gmail.com>
2022-02-27 23:13:46 -0500

    start Idea matcher

---
+++ phrasematcher/phrase_matcher.py
@@ -0,0 +1,10 @@
+class OpinionMatcher:
+    """this class is supposed to find if student express their own interest or not."""
+    def __init__(self, ):
+        self.phrases_to_check = ["In my opnion", "I think", "I believe", "In conclusion"]
+    
+    def match_against(self, text: str) -> bool:
+        for phrase in self.phrases_to_check:
+            pass
+
+        return False
