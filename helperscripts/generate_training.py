5ea7f6c1bc837bd891129a36f99b9d776a6a7cc4
JerrisonChang <jerrisonChang@gmail.com>
2022-04-29 00:53:00 -0400

    updates

--- helperscripts/generate_training.py
+++ helperscripts/generate_training.py
@@ -1,7 +1,8 @@
 import os
+from glob import glob
 from enum import Enum
 from dataclasses import dataclass
-# from typing import List
+from typing import List
 
 import pandas  as pd
 
@@ -28,26 +29,43 @@ TOPIC_INDEPENTENT = {
     Catagories.BIBLIOGRAPHY: False
 }
 
+class ExtensionError(Exception):
+    pass
 
-HIST_SEMESTERS = ['fa20', 'fa21', 'sp22', 'summer21']
+# HIST_SEMESTERS = ['fa20', 'fa21', 'sp22', 'summer21']
 FOLDER = os.path.join('./gradebook/final')
 HWS = [
     HW('normative_ethics', 'Normative Ethics'),
     HW('privacy', 'Privacy'),
     HW('epfoscl', 'Ethics, privacy, freedom of speech, copyrights, and laws'),
     HW('hacktivism', 'Hacktivism'),
-    HW('facial_recognition', "Facial Recognition")
+    HW('facial_recognition', "Facial Recognition") # usually extra assignment
 ]
+
+def read_spread_sheet(path: str) -> pd.DataFrame:
+    _, extension = os.path.splitext(path)
+    
+    if extension == '.csv':
+        return pd.read_csv(path, encoding='utf-8')
+    elif extension == '.xlsx':
+        return pd.read_excel(path, engine="openpyxl")
+    else:
+        raise ExtensionError(f"Unsupported extension type; expected: .csv or .xlsx, got {extension}")
 
 def get_df(catagory: Catagories, hw_code: str) -> pd.DataFrame:
     if TOPIC_INDEPENTENT[catagory]:
-        print("just this")
-        file_names = [f"{hw_code}_{i}" for i in HIST_SEMESTERS] # file names starting with ...
-        df = pd.DataFrame()
+        files = glob(os.path.join(FOLDER, f"{hw_code}_*"))
     else:
-        print("grab all")
+        files = glob(FOLDER)
 
+    frames = [read_spread_sheet(i) for i in files]
+    result = pd.concat(frames)[[catagory.value, 'title page', 'essay body', 'essay refs', 'word count']]
+    
+    return result
 
 if __name__=="__main__":
-    get_df(Catagories.RESEARCH, 'normative_ethics')
+    result = get_df(Catagories.CONTENT, 'normative_ethics')
+
+    print(result[:30])
+
     
